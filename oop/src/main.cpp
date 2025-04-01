5772983b37bbca091a21eddc5980f2ceee4a7a39
Alexey <lesha29kartashev@yandex.ru>
2025-04-01 11:34:25 +0300

    feat: add example of override method

--- oop/src/main.cpp
+++ oop/src/main.cpp
@@ -4,14 +4,27 @@
 #include "Child.hpp"
 #include "AnotherChild.hpp"
 #include "StepChild.hpp"
+#include "ThirdChild.hpp"
 
 int main()
 {
+    ///////////////////////////////////////////
+    // Diamond problem
+    ///////////////////////////////////////////
+
     StepChild c;
   
     c.Say(); 
-    c.AnotherChild::Say();
     static_cast<Child&>(c).Say();
+    c.AnotherChild::Say();
+
+    ///////////////////////////////////////////
+    // Early and latest linking
+    ///////////////////////////////////////////
+
+    Parent* third = new ThirdChild();
+    third->Do();
+    third->AnotherDo();
 
     return 0;
 }
